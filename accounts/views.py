33e7102b0a62606d036452c516e2f1da5c105a0b
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-09-09 01:06:26 +0530

    urls files added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
-# Create your views here.
+
+def AccountHome(request):
+    return HttpResponse("Account Home")
