5664939ee87e85faf7197458cb32582f4db1f90e
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-09-12 11:09:15 +0530

    cart and cartItems models created in orders app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Cart,CartItems
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user','created_at']
+
+@admin.register(CartItems)
+class CartItemsAdmin(admin.ModelAdmin):
+    list_display = ['cart' , 'product','quantity','price']
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+class Cart(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+class CartItems(models.Model):
+    
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10 , decimal_places=2)
+
