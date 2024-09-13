7607a35d3ef34cd00a0b5ecbab4118ecb1992aad
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-09-13 13:40:22 +0530

    add to cart functionality added

--- ZypherCart/urls.py
+++ ZypherCart/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
     path('accounts/',include('accounts.urls')),
-    path('products/',include('products.urls'))
+    path('products/',include('products.urls')),
+    path('orders/',include('orders.urls'))
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
